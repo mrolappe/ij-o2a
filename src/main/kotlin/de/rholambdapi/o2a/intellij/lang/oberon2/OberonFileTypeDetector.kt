ac2946ee459c7b38a2d64240c70b6c6c45385ad3
Marco Rolappe <215132+mrolappe@users.noreply.github.com>
2024-07-21 14:27:12 +0200

    WIP file type detector

---
+++ src/main/kotlin/de/rholambdapi/o2a/intellij/lang/oberon2/OberonFileTypeDetector.kt
@@ -0,0 +1,27 @@
+package de.rholambdapi.o2a.intellij.lang.oberon2
+
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.fileTypes.FileTypeRegistry.FileTypeDetector
+import com.intellij.openapi.util.io.ByteSequence
+import com.intellij.openapi.vfs.VirtualFile
+
+private val moduleRegex = Regex("MODULE[ ]+\\p{Alpha}\\w*;")
+
+class OberonFileTypeDetector: FileTypeDetector {
+    override fun detect(file: VirtualFile, firstBytes: ByteSequence, firstCharsIfText: CharSequence?): FileType? {
+        println("OberonFileTypeDetector, detect; file: $file, extension: ${file.extension}")
+
+        if (file.extension == null || firstCharsIfText == null) {
+            return null
+        }
+
+        val fileType = if (file.extension.equals(".mod") && firstCharsIfText.contains(moduleRegex)) {
+            OberonFileType.INSTANCE
+        } else {
+            null
+        }
+
+        println("OberonFileTypeDetector, detect; file: $file -> fileType: $fileType")
+        return fileType
+    }
+}
