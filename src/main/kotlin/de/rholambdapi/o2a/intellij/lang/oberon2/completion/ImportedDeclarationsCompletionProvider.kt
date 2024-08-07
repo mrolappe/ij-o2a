4ac6b86317118e56cc44e4f039636c33afda8868
Marco Rolappe <215132+mrolappe@users.noreply.github.com>
2024-08-07 20:39:19 +0200

    WIP(ImportedDeclarationsCompletionProvider)

--- src/main/kotlin/de/rholambdapi/o2a/intellij/lang/oberon2/completion/ImportedDeclarationsCompletionProvider.kt
+++ src/main/kotlin/de/rholambdapi/o2a/intellij/lang/oberon2/completion/ImportedDeclarationsCompletionProvider.kt
@@ -3,8 +3,14 @@ package de.rholambdapi.o2a.intellij.lang.oberon2.completion
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.descendantsOfType
+import com.intellij.psi.util.elementType
 import com.intellij.psi.util.prevLeaf
 import com.intellij.util.ProcessingContext
+import de.rholambdapi.o2a.intellij.lang.oberon2.importedModulesAndAliases
+import de.rholambdapi.o2a.intellij.lang.oberon2.psi.*
 
 class ImportedDeclarationsCompletionProvider : CompletionProvider<CompletionParameters>() {
     override fun addCompletions(
@@ -12,16 +18,50 @@ class ImportedDeclarationsCompletionProvider : CompletionProvider<CompletionPara
         context: ProcessingContext,
         result: CompletionResultSet
     ) {
-        val leafBeforePosition = parameters.position.prevLeaf()
-        println("leafBeforePosition: $leafBeforePosition, text: ${leafBeforePosition?.text}, offs: ${leafBeforePosition?.textOffset}")
+        val position = parameters.position
+        val prefix = result.prefixMatcher.prefix
 
-        if (leafBeforePosition == null
+        val text = "imported declarations; prefix: $prefix"
+
+        // requires the parser to be robust enough to yield the node-to-be in presence of an error due to incomplete input
+        if (/*prefix.isEmpty() &&*/ position.prevSibling?.parent is OberonQualIdentQualified) {
+            // LHS of . -> module/alias name
+            val qualifier = position.prevLeaf { it.elementType == OberonTypes.IDENT }?.text!!
+
+            // TODO use indexing infrastructure
+            val modulesAndAliases = position.oberonFile?.importedModulesAndAliases
+            modulesAndAliases?.entries
+                ?.filter { (modules, alias) ->
+                    val matches = modules.any { it.name == qualifier } || alias?.name == qualifier
+                    println("qual $qualifier matches: $matches")
+                    matches
+                }
+                ?.flatMap { (modules, _) -> modules }
+                ?.flatMap { module ->
+                    println("module: $module")
+                    module.exportedProcedures
+                        .filter { val procDeclName = it.procDeclName
+                            val nameText = procDeclName.text
+                            val startsWith = nameText.startsWith(prefix)
+                            println("decl name: $procDeclName, nameText: $nameText, startsWith: $startsWith")
+                            startsWith
+                        }
+                        .map { LookupElementBuilder.create(it) }
+
+//                    module.exportedOberonASharedLibProcedures
+                }
+                ?.also { elements -> result.addAllElements(elements) }
+
+//            result.addElement(LookupElementBuilder.create("import declarations aus ${position.prevLeaf { it.elementType == OberonTypes.IDENT }?.text}"))
+        }
+        result.addElement(LookupElementBuilder.create(text))
+
+        if (position == null
 //            || (leafBeforePosition.elementType != OberonTypes.DOT && !leafBeforePosition.prevSibling.isIdentifier())
         ) {
             return
         }
 
-        // prüfen auf . und IDENT davor
         // falls vorhanden, liste der importierten module ermitteln
         // prüfen, ob IDENT in der liste vorkommt
         // falls nein, fertig; falls ja, top level member des ermittelten moduls ermitteln und lookup elements ableiten
@@ -37,3 +77,24 @@ class ImportedDeclarationsCompletionProvider : CompletionProvider<CompletionPara
     }
 
 }
+
+val PsiElement.oberonFile: OberonFile?
+    get() = if (this is OberonFile) {
+        this
+    } else {
+        this.containingFile.descendantsOfType<OberonFile>().firstOrNull()
+    }
+
+val OberonModuleDef.exportedProcedures: Set<OberonProcedureDecl>
+    get() = topLevelDecls.procedureDeclList
+        .filter { procedure ->
+            val name = procedure.name
+            val markedForExport = procedure.markedForExport()
+            println("exportedProcedures, name: $name, export: $markedForExport")
+            markedForExport
+        }
+        .toSet()
+
+//val OberonModuleDef.exportedOberonASharedLibProcedures: Set<OberonOberonALibProcDecl>
+//    get() = topLevelDecls.oberonALibProcDeclList
+//        .filter { procDecl -> procDecl }
