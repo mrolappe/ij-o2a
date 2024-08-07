83d793dc0af555877292958f13fb276b054848af
Marco Rolappe <215132+mrolappe@users.noreply.github.com>
2024-08-07 20:46:44 +0200

    WIP(OberonDocumentationProvider)

--- src/main/kotlin/de/rholambdapi/o2a/intellij/lang/oberon2/OberonDocumentationProvider.kt
+++ src/main/kotlin/de/rholambdapi/o2a/intellij/lang/oberon2/OberonDocumentationProvider.kt
@@ -3,32 +3,44 @@ package de.rholambdapi.o2a.intellij.lang.oberon2
 import com.intellij.lang.documentation.AbstractDocumentationProvider
 import com.intellij.lang.documentation.DocumentationMarkup
 import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.util.text.HtmlBuilder
+import com.intellij.openapi.util.text.HtmlChunk
+import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiDocCommentBase
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
+import com.intellij.psi.javadoc.PsiDocComment
 import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.descendants
+import com.intellij.psi.util.descendantsOfType
+import com.intellij.psi.util.elementType
+import de.rholambdapi.o2a.intellij.Autodoc
 import de.rholambdapi.o2a.intellij.AutodocLookup
+import de.rholambdapi.o2a.intellij.FUNCTION
 import de.rholambdapi.o2a.intellij.lang.oberon2.psi.*
 import java.nio.file.Paths
+import java.util.function.Consumer
 
 class OberonDocumentationProvider : AbstractDocumentationProvider() {
-    private val LOG = thisLogger()
+    private val log = thisLogger()
 
-    private val autodocLookup = AutodocLookup(Paths.get("/Users/mrolappe/studio/rlp-ami-vbcc-dev/NDK3.2/Autodocs"))
+    private val autodocLookup = AutodocLookup(Paths.get("/Users/mrolappe/studio/rlp-ami-vbcc-dev/NDK3.2/Autodocs")) // TODO path via config
 
     override fun getQuickNavigateInfo(element: PsiElement?, originalElement: PsiElement?): String {
         val msg = "getQuickNavigateInfo, element: $element, originalElement: $originalElement"
-        LOG.debug(msg)
+        log.debug(msg)
         return msg
     }
 
     override fun getUrlFor(element: PsiElement?, originalElement: PsiElement?): List<String> {
-        LOG.debug("getUrlFor, super.getUrlFor: ${super.getUrlFor(element, originalElement)}, element: $element, originalElement: $originalElement")
+        log.debug("getUrlFor, super.getUrlFor: ${super.getUrlFor(element, originalElement)}, element: $element, originalElement: $originalElement")
         return listOf("https://rolappe.it/uno", "https://rolappe.it/dos")
     }
 
     override fun generateDoc(element: PsiElement?, originalElement: PsiElement?): String {
-        LOG.debug("generateDoc, element: $element, originalElement: $originalElement")
+        log.debug("generateDoc, element: $element, originalElement: $originalElement")
 
         return when {
             element is OberonImportAlias -> {
@@ -36,15 +48,20 @@ class OberonDocumentationProvider : AbstractDocumentationProvider() {
                 "Import alias for module ${module?.ident?.text}"
             }
 
-            element is OberonIdentDef && element.parent is OberonAmigaOberonSharedLibProcDecl -> autodocFunctionDescriptionByProcName(element.ident.text)
+            element is OberonIdentDef && element.parent is OberonAmigaOberonSharedLibProcDecl ->
+                autodocFunctionDescriptionByProcName(Autodoc.ElementName(element.ident.text))
             element is OberonProcDeclName -> lookupAutoDoc(element, originalElement)
             else -> "doc for element $element, original element $originalElement"
         }
     }
 
-    private fun autodocFunctionDescriptionByProcName(procName: String): String {
+    private fun autodocFunctionDescriptionByProcName(procName: Autodoc.ElementName): String {
         val functionDescription = autodocLookup.autodocFunctionDescriptionOrNull(procName)
 
+        return buildDocString(procName, functionDescription)
+    }
+
+    private fun buildDocString(procName: Autodoc.ElementName, functionDescription: String?): String {
         val docString = buildString {
             append("autodoc function description for $procName\n$functionDescription")
 
@@ -96,25 +113,43 @@ class OberonDocumentationProvider : AbstractDocumentationProvider() {
         return docString
     }
 
+    override fun getCustomDocumentationElement(
+        editor: Editor,
+        file: PsiFile,
+        contextElement: PsiElement?,
+        targetOffset: Int
+    ): PsiElement? {
+//        println("getCustomDocumentationElement, contextElement: $contextElement")
+        return null
+    }
+
     private fun lookupAutoDoc(element: OberonProcDeclName, originalElement: PsiElement?): String {
         val procName = element.procedureName.text
 
-        val functionDescription = autodocLookup.autodocFunctionDescriptionOrNull(procName)
+        val elementName = Autodoc.ElementName(procName)
+        val autodocs = autodocLookup.getAutodocsByFunctionName(elementName)
+        val function = autodocs.firstOrNull()?.contentForSection(elementName, FUNCTION)?.asString ?: "???"
 
-        return "TODO look up autodoc for ${element.procedureName.text}<br/>element: $element<br/>original element: $originalElement<br/>" +
-                "element parent: ${element.parent}<br/>text from autodoc:<br/>$functionDescription"
+        return buildDocString(elementName, function)
+//        return "TODO look up autodoc for ${element.procedureName.text}<br/>element: $element<br/>original element: $originalElement<br/>" +
+//                "element parent: ${element.parent}<br/>text from autodoc:<br/>$function"
     }
 
     override fun generateHoverDoc(element: PsiElement, originalElement: PsiElement?): String {
         val msg = "hover doc, element: $element, originalElement: $originalElement"
-        LOG.debug(msg)
+        log.debug(msg)
         return msg
     }
 
     override fun generateRenderedDoc(comment: PsiDocCommentBase): String {
         val msg = "generateRenderedDoc, comment: $comment"
-        LOG.debug(msg)
-        return msg
+        log.debug(msg)
+        val doc = HtmlBuilder()
+            .append(DocumentationMarkup.CENTERED_ELEMENT)
+            .append("Pseudo comment: ").append(HtmlChunk.tag("code").addText(comment.text))
+            .wrapWithHtmlBody()
+            .toString()
+        return doc
     }
 
     override fun getDocumentationElementForLookupItem(
@@ -122,7 +157,7 @@ class OberonDocumentationProvider : AbstractDocumentationProvider() {
         `object`: Any?,
         element: PsiElement?
     ): PsiElement? {
-        LOG.debug("getDocumentationElementForLookupItem, object: $`object`, element: $element")
+        log.debug("getDocumentationElementForLookupItem, object: $`object`, element: $element")
         return super.getDocumentationElementForLookupItem(psiManager, `object`, element)
     }
 
@@ -131,8 +166,17 @@ class OberonDocumentationProvider : AbstractDocumentationProvider() {
         link: String?,
         context: PsiElement?
     ): PsiElement? {
-        LOG.debug("getDocumentationElementForLink, link: $link, context: $context")
+        log.debug("getDocumentationElementForLink, link: $link, context: $context")
         return super.getDocumentationElementForLink(psiManager, link, context)
+    }
+
+    override fun collectDocComments(file: PsiFile, sink: Consumer<in PsiDocCommentBase>) {
+        file.descendantsOfType<PsiComment>().firstOrNull { it.elementType == OberonTypes.PSEUDO_COMMENT }
+            ?.let { sink.accept(MyDocComment(it)) }
     }
 }
 
+class MyDocComment(private val pseudoComment: PsiComment) : PsiDocCommentBase, PsiComment by pseudoComment {
+    override fun getOwner() = pseudoComment
+
+}
