e3245e3e3e0fbf72b77f49fdd899210ec855f5c7
Marco Rolappe <215132+mrolappe@users.noreply.github.com>
2024-08-12 13:36:40 +0200

    feat: Oberon structure view

--- src/main/kotlin/de/rholambdapi/o2a/intellij/lang/oberon2/OberonStructureView.kt
+++ src/main/kotlin/de/rholambdapi/o2a/intellij/lang/oberon2/OberonStructureView.kt
@@ -2,32 +2,30 @@ package de.rholambdapi.o2a.intellij.lang.oberon2
 
 import com.intellij.icons.AllIcons
 import com.intellij.icons.AllIcons.Nodes
-import com.intellij.ide.projectView.PresentationData
 import com.intellij.ide.structureView.*
 import com.intellij.ide.util.treeView.smartTree.*
 import com.intellij.lang.PsiStructureViewFactory
 import com.intellij.navigation.ItemPresentation
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.NavigatablePsiElement
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
 import de.rholambdapi.o2a.intellij.lang.oberon2.psi.*
-import de.rholambdapi.o2a.intellij.lang.oberon2.psi.impl.OberonModuleDefImpl
 
 class OberonStructureViewFactory : PsiStructureViewFactory {
     override fun getStructureViewBuilder(psiFile: PsiFile): StructureViewBuilder {
+        require(psiFile is OberonFile) { "PSI file must be on Oberon file" }
         return object : TreeBasedStructureViewBuilder() {
             override fun createStructureViewModel(editor: Editor?): StructureViewModel {
-                return OberonStructureViewModel(psiFile, editor)
+                return OberonStructureViewModel(psiFile as OberonFile, editor)
             }
 
         }
     }
 }
 
-class OberonStructureViewModel(psiFile: PsiFile, editor: Editor?) :
-    StructureViewModelBase(psiFile, editor, OberonStructureViewElement(psiFile)),
+class OberonStructureViewModel(file: OberonFile, editor: Editor?) :
+    StructureViewModelBase(file, editor, FileNode(file)),
     StructureViewModel.ElementInfoProvider {
 
     init {
@@ -35,7 +33,7 @@ class OberonStructureViewModel(psiFile: PsiFile, editor: Editor?) :
             OberonConstDecl::class.java,
             OberonModuleDef::class.java,
             OberonProcedureDecl::class.java,
-            OberonOberonALibProcDecl::class.java,
+//            OberonOberonALibProcDecl::class.java,
             OberonTypeDecl::class.java
         )
     }
@@ -45,7 +43,8 @@ class OberonStructureViewModel(psiFile: PsiFile, editor: Editor?) :
     override fun isAlwaysLeaf(element: StructureViewTreeElement?) = when (element) {
         is OberonConstDecl, is OberonTypeDecl, is OberonVarDecl,
         is OberonConstDeclName, is OberonTypeDeclName, is OberonVarDeclName,
-        is OberonOberonALibProcDecl -> true
+            /*is OberonOberonALibProcDecl*/
+            -> true
 
         else -> false
     }
@@ -69,95 +68,83 @@ abstract class BaseNode<T : NavigatablePsiElement>(protected val element: T) : S
     override fun getValue() = element
 }
 
-class OberonStructureViewElement(element: NavigatablePsiElement, private val separateEntryPerSection: Boolean = false) :
-    BaseNode<NavigatablePsiElement>(element), SortableTreeElement {
+data class FileNode(val file: OberonFile) : BaseNode<NavigatablePsiElement>(file), SortableTreeElement {
     override fun getPresentation(): ItemPresentation {
-        // TODO
-        return element.presentation ?: PresentationData().apply {
-            presentableText = when (element) {
-                is OberonConstDeclName -> element.constantName.text
-                is OberonModuleDef -> (element as OberonModuleDefImpl).name
-                is OberonModuleInit -> "<module init>"
-                is OberonOberonALibProcDecl -> element.procDeclName.procedureName.text
-                is OberonTypeDeclName -> element.typeName.text
-                is OberonVarDeclName -> element.varName.text
-                else -> "$element"
-            }
-
-            when (element) {
-                is OberonConstDeclName -> AllIcons.Nodes.Constant
-                is OberonModuleDef -> AllIcons.Nodes.Module
-                is OberonModuleInit -> AllIcons.Nodes.ClassInitializer
-                is OberonProcedureDecl, is OberonOberonALibProcDecl -> AllIcons.Nodes.Function
-                is OberonTypeDeclName -> AllIcons.Nodes.Type
-                is OberonVarDeclName -> AllIcons.Nodes.Variable
-                else -> null
-            }?.let { setIcon(it) }
+        return object : ItemPresentation {
+            override fun getPresentableText() = "File ${file.name}"
+            override fun getIcon(unused: Boolean) = OberonIcons.MODULE
         }
     }
 
     override fun getChildren(): Array<TreeElement> {
-        return when (element) {
-            is OberonFile ->
-                PsiTreeUtil.getChildrenOfTypeAsList(element, OberonModuleDef::class.java)
-                    .map { OberonStructureViewElement(it) }
-                    .toTypedArray()
-
-            is OberonModuleDef -> {
-                val treeElements = mutableListOf<TreeElement>()
-
-                element.moduleInit?.let { treeElements.add(ModuleInitNode(it)) }
-
-                // TODO separate entry per section? as option?
-                val varSections = element.topLevelDecls.varSectionList
-
-                if (separateEntryPerSection) {
-                    varSections
-                        .map { section -> section.varDeclList }
-                        .map { varDecls -> varDecls.map { it.varDeclNameList.varDeclNameList }.mapTo(treeElements) { VarsNode(it) } }
-                } else {
-                    varSections
-                        .flatMap { it.varDeclList }
-                        .mapTo(treeElements) { VarsNode(it.varDeclNameList.varDeclNameList) }
-                }
-
-                element.topLevelDecls.procedureDeclList
-                    .mapTo(treeElements) { ProcedureNode(it) }
-
-                if (separateEntryPerSection) {
-                    element.topLevelDecls.constSectionList
-                        .mapTo(treeElements) { ConstSectionNode(it.constDeclList) }
-                } else {
-                   element.topLevelDecls.constSectionList
-                       .flatMap { it.constDeclList }
-                       .toList()
-                       .also { treeElements.add(ConstSectionNode(it)) }
-                }
-
-                if (separateEntryPerSection) {
-                    element.topLevelDecls.typeSectionList
-                        .filter { it.typeDeclList.isNotEmpty() }
-                        .mapTo(treeElements) { TypeSectionNode(it.typeDeclList) }
-                } else {
-                    element.topLevelDecls.typeSectionList
-                        .flatMap { it.typeDeclList }
-                        .toList()
-                        .also { if (it.isNotEmpty()) treeElements.add(TypeSectionNode(it)) }
-                }
-
-                treeElements.toTypedArray()
-            }
-
-            is OberonProcedureDecl ->
-                PsiTreeUtil.getChildrenOfTypeAsList(element, OberonProcedureDecl::class.java)
-                    .map { ProcedureNode(it) }
-                    .toTypedArray()
-
-            else -> TreeElement.EMPTY_ARRAY
-        }
+        return PsiTreeUtil.getChildrenOfTypeAsList(element, OberonModuleDef::class.java)
+            .map { ModuleNode(it) }
+            .toTypedArray()
     }
 
     override fun getAlphaSortKey() = element.name ?: ""
+}
+
+class ModuleNode(
+    module: OberonModuleDef,
+    private val separateEntryPerSection: Boolean = false
+) : BaseNode<OberonModuleDef>(module) {
+    override fun getPresentation(): ItemPresentation {
+        return object : ItemPresentation {
+            override fun getPresentableText() = "Module ${element.name}"
+            override fun getIcon(unused: Boolean) = Nodes.Package
+        }
+    }
+
+    override fun getChildren(): Array<TreeElement> {
+        val treeElements = mutableListOf<TreeElement>()
+
+        element.moduleInit?.let { treeElements.add(ModuleInitNode(it)) }
+
+        // TODO separate entry per section? as option?
+        val varSections = element.topLevelDecls.varSectionList
+
+        if (separateEntryPerSection) {
+            varSections
+                .map { section -> section.varDeclList }
+                .map { varDecls ->
+                    varDecls.map { it.varDeclNameList.varDeclNameList }.mapTo(treeElements) { VarsNode(it) }
+                }
+        } else {
+            varSections
+                .flatMap { it.varDeclList }
+                .mapTo(treeElements) { VarsNode(it.varDeclNameList.varDeclNameList) }
+        }
+
+        element.topLevelDecls.procedureDeclList.also {
+            if (it.isNotEmpty()) {
+                treeElements.add(ProceduresNode(it))
+            }
+        }
+
+        if (separateEntryPerSection) {
+            element.topLevelDecls.constSectionList
+                .mapTo(treeElements) { ConstSectionNode(it.constDeclList) }
+        } else {
+            element.topLevelDecls.constSectionList
+                .flatMap { it.constDeclList }
+                .toList()
+                .also { treeElements.add(ConstSectionNode(it)) }
+        }
+
+        if (separateEntryPerSection) {
+            element.topLevelDecls.typeSectionList
+                .filter { it.typeDeclList.isNotEmpty() }
+                .mapTo(treeElements) { TypeSectionNode(it.typeDeclList) }
+        } else {
+            element.topLevelDecls.typeSectionList
+                .flatMap { it.typeDeclList }
+                .toList()
+                .also { if (it.isNotEmpty()) treeElements.add(TypeSectionNode(it)) }
+        }
+
+        return treeElements.toTypedArray()
+    }
 }
 
 class ModuleInitNode(moduleInit: OberonModuleInit) : BaseNode<OberonModuleInit>(moduleInit) {
@@ -171,10 +158,10 @@ class ModuleInitNode(moduleInit: OberonModuleInit) : BaseNode<OberonModuleInit>(
     override fun getChildren(): Array<TreeElement> = TreeElement.EMPTY_ARRAY
 }
 
-class ConstNode(constDecl: OberonConstDecl) : BaseNode<OberonConstDecl>(constDecl) {
+class ConstNode(constDecl: OberonConstDeclName) : BaseNode<OberonConstDeclName>(constDecl) {
     override fun getPresentation(): ItemPresentation {
         return object : ItemPresentation {
-            override fun getPresentableText() = element.constantNameAsString
+            override fun getPresentableText() = element.name
             override fun getIcon(unused: Boolean) = Nodes.Constant
         }
     }
@@ -190,7 +177,7 @@ data class ConstSectionNode(private val elements: List<OberonConstDecl>) : Struc
         }
     }
 
-    override fun getChildren(): Array<TreeElement> = elements.map { ConstNode(it) }.toTypedArray()
+    override fun getChildren(): Array<TreeElement> = elements.map { ConstNode(it.constDeclName) }.toTypedArray()
     override fun getValue() = elements
 }
 
@@ -248,13 +235,25 @@ val OberonConstDecl.constantNameAsString
 val OberonModuleDef.moduleInit
     get() = moduleTail.moduleInit
 
-val PsiElement.procedureName
+val Oberon2ProcedureElement.procedureName
     get() = when (this) {
         is OberonProcedureDecl -> procDeclName.procedureName.text
-        is OberonOberonALibProcDecl -> procDeclName.procedureName.text
+        is OberonOberonALibProcDecl -> identDef.ident.text
         else -> null
     }
 
+data class ProceduresNode(private val elements: List<Oberon2ProcedureElement>) : StructureViewTreeElement {
+    override fun getPresentation(): ItemPresentation {
+        return object : ItemPresentation {
+            override fun getPresentableText() = "Procedures"
+            override fun getIcon(unused: Boolean) = null
+        }
+    }
+
+    override fun getChildren(): Array<TreeElement> = elements.map { ProcedureNode(it) }.toTypedArray()
+
+    override fun getValue() = elements
+}
 
 class ProcedureNode(element: Oberon2ProcedureElement) : BaseNode<Oberon2ProcedureElement>(element) {
     private val presentation: ItemPresentation
@@ -262,7 +261,7 @@ class ProcedureNode(element: Oberon2ProcedureElement) : BaseNode<Oberon2Procedur
     init {
         val text = when (element) {
             is OberonProcedureDecl -> element.procDeclName.procedureName.text
-            is OberonOberonALibProcDecl -> element.procDeclName.procedureName.text
+            is OberonOberonALibProcDecl -> element.procedureName
             is OberonOberonAExternalProcDecl -> element.procDeclName.procedureName.text
             is OberonAmigaOberonExternalProcDecl -> element.identDef.nameIdentifier.text    // TODO
             else -> null!!
@@ -285,7 +284,7 @@ class ProcedureNode(element: Oberon2ProcedureElement) : BaseNode<Oberon2Procedur
                     .toTypedArray<TreeElement>()
             }
 
-            is OberonOberonALibProcDecl -> TreeElement.EMPTY_ARRAY
+//            is OberonOberonALibProcDecl -> TreeElement.EMPTY_ARRAY
             else -> throw IllegalArgumentException(element.toString())
         }
     }
